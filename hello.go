05c92144dd583e020c2f24764f8f43e76c38681c
Paulo <paulosantos@id.uff.br>
2022-04-06 22:15:52 -0300

    feat: funcao monitorar site alura

--- hello.go
+++ hello.go
@@ -1,23 +1,40 @@
 package main
 
 // começando testes em go
+// bibliotecas padroes https://pkg.go.dev/std
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	nome := "Paulo"
-	versao := 1.1
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa esta na versao", versao)
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
+	exibeIntroducao()
+	// nao existe while true em go. for se condicao é equivalente
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("O endereço da minha variavel comando é", &comando)
-	fmt.Println("O comando escolhido foi", comando)
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		// nao precisa break no switch
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Até Logo! ;)")
+			os.Exit(0)
+		// utilizamos -1 no os.exit p mostrar que saimos de forma inesperada.
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
+
+	}
 
 	// if comando == 1 {
 	// 	fmt.Println("Monitorando...")
@@ -29,15 +46,41 @@ func main() {
 	// 	fmt.Println("Não conheço este comando")
 	// }
 
-	// nao precisa break no switch
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 3:
-		fmt.Println("Saindo..")
-	default:
-		fmt.Println("Não conheço este comando")
+}
+
+func exibeIntroducao() {
+	nome := "Paulo"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa esta na versao", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	// _ serve para ignorar a variavel q nao quer neste caso o err
+	resp, _ := http.Get(site)
+	fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("O Site:", site, "foi carregado corretamente")
+	} else {
+		fmt.Println("O Site:", site, "esta com problema")
 	}
+	// da p trazer conteudo?
+	// https://higordiego.com.br/posts/golang-jira/
 }
