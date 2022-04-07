cd47f4c1318ce4666b2d644df5122b3d0e97a62c
Paulo <paulosantos@id.uff.br>
2022-04-06 21:14:14 -0300

    feat: cases

--- hello.go
+++ hello.go
@@ -16,4 +16,26 @@ func main() {
 	fmt.Scan(&comando)
 	fmt.Println("O endereço da minha variavel comando é", &comando)
 	fmt.Println("O comando escolhido foi", comando)
+
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo Logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo..")
+	// } else {
+	// 	fmt.Println("Não conheço este comando")
+	// }
+
+	// nao precisa break no switch
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 3:
+		fmt.Println("Saindo..")
+	default:
+		fmt.Println("Não conheço este comando")
+	}
 }
